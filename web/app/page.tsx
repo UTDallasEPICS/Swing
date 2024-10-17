b49e48989acda265026d98ac590683ae8e278c4a
SwiftCode123 <dhruv.tr03@gmail.com>
2024-10-17 15:55:42 -0500

    homepage created

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,19 +1,21 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'; 
+import Logo from './components/logo'; 
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.logoContainer}>
-        <Image
-          className={styles.logo}
-          src="/swingLogo.svg"
-          alt="Next.js Logo"
-          width={500}
-          height={150}
-          priority
-        />
+      <div className={styles.logoContainer}> 
+        <Logo /> 
         </div>
+        
+        {/* <Link href="/about">
+        <button className={styles.button}>
+          Go to Home Page
+        </button>
+      </Link> */}
+
     </main>
   )
 }
