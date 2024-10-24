c4d5b138219e94babf93ef87d430f698e5057852
Jahnzeb Bastaki <jahnzebbastaki@gmail.com>
2024-10-24 16:19:30 -0500

    fixed minor problem

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,6 @@
-import Logo from './components/logo'
-import styles from './page.module.css'
+import Logo from './components/logo';
+import styles from './page.module.css';
 import Link from 'next/link'; 
-import Logo from './components/logo'; 
 
 export default function Home() {
   return (
