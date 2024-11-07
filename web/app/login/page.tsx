c1ea1480754e48adcc9de3f40d5736eb187c4fbb
ekandikattu <eeshan05@gmail.com>
2024-11-07 16:49:06 -0600

    added tailwind.css to login page

--- web/app/login/page.tsx
+++ web/app/login/page.tsx
@@ -2,53 +2,55 @@
 
 import Logo from '../components/logo'
 import styles from '../login.module.css'
+import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 // import {useState} from 'react'
 
 export default function LoginPage() {
   // Store user data
   // const [email, setEmail] = useState('');
   // const [password, setPassword] = useState('');
-
+  const router = useRouter();
   // Handle form submission
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const isLoggedIn = true;
     e.preventDefault();
     console.log('login pressed!');
     // Will implement page rerouting here
+    router.push('/');
+    
   };
 
   return (
-    <div className={styles.main}>
-      <Logo/>
-      <form onSubmit={handleSubmit} className={styles.loginFields}>
-        <div className={styles.loginText}>
+    <div className="flex flex-col items-center p-12 min-h-screen bg-white">
+      <Logo />
+      <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center text-center pt-12 pb-12">
+        
+        <div className="text-black flex justify-center items-center text-center pb-12">
           <h3>Log in to your account</h3>
         </div>
 
-        <label htmlFor="email" className={styles.labelText}>Email address</label>
+        <label htmlFor="email" className="w-full text-left p-1">Email address</label>
         <input
           type="email"
           id="email"
           name="email"
-          // value={email}
-          // onChange={(e) => setEmail(e.target.value)}
           required
           placeholder="Enter your email"
-          className={styles.inputFields}
+          className="text-black mb-8 p-4 rounded-lg border border-black w-80 shadow-lg"
         />
 
-        <label htmlFor="password" className={styles.labelText}>Password</label>
+        <label htmlFor="password" className="w-full text-left p-1">Password</label>
         <input
           type="password"
           id="password"
           name="password"
-          // value={password}
-          // onChange={(e) => setPassword(e.target.value)}
           required
           placeholder="Enter your password"
-          className={styles.inputFields}
+          className="text-black mb-8 p-4 rounded-lg border border-black w-80 shadow-lg"
         />
 
-        <button type="submit" className={styles.loginButton}>
+        <button type="submit" className="text-black mt-12 p-4 rounded-lg border border-black w-80 shadow-lg bg-red-700">
           Login
         </button>
       </form>
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,14 +1,57 @@
-import Logo from './components/logo'
-import styles from './page.module.css'
-import Link from 'next/link'; 
-import Logo from './components/logo'; 
+// import Logo from './components/logo'
+// import styles from './page.module.css'
+// import Link from 'next/link'; 
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <main className={styles.main}>
+//       <div className={styles.logoContainer}>
+//       <Logo/>
+//       </div>
+//     </main>
+//   )
+// }
+"use client";
+import Link from 'next/link';
+import Logo from './components/logo'; 
+import styles from './homepage.module.css'
+import { redirect } from 'next/navigation';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+export default function Homepage() {
+  useEffect
+  var isLoggedIn = false;
+  console.log('in root page!');
+  console.log('loggedIn val: ' + isLoggedIn);
+  if (!isLoggedIn){
+    isLoggedIn = true;
+    redirect('login');
+  }
   return (
     <main className={styles.main}>
-      <div className={styles.logoContainer}>
-      <Logo/>
+      {/* Imported Logo since it's going to be the same everywhere*/}
+      <Logo />
+      <div className={styles.videoUploadText}>
+          <h2>Click below to upload your
+              video after you have recorded it</h2>
+      </div>
+      
+      {/* For the arrow */}
+      <div className={styles.arrowContainer}>
+        <div className={styles.line}></div>
+        <div className={styles.arrow}></div>
+      </div>
+
+      {/* Video Upload Button - redirects to video upload page */}
+      <div className={styles.videoButtonContainer}>
+        <Link href="/homepage">
+        <button className={styles.videoUploadButton}>
+          Go to Video Upload Page
+        </button>
+      </Link>
       </div>
     </main>
-  )
+  );
 }
+  
