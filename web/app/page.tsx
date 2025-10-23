4bf07680fb019624c78d41471d1ea562f3ce9388
Annie <ttn210006@utdallas.edu>
2025-10-23 16:32:48 -0500

    add view patient history button
    page 4 update, needs to make history table

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -42,8 +42,18 @@ export default function Home() {
       </div>
 
       {/* Upload Button */}
-      <div className="flex flex-col justify-center items-center mt-5">
-        <Link href="/videoUpload">
+    <div className ="flex justify-end items-center mt-5 gap-4">
+           <Link href="/patientHistory">
+          <button className="px-8 py-4 bg-black hover:bg-gray-200 text-white rounded-lg 
+            cursor-pointer text-lg font-semibold transition duration-300
+            shadow-lg hover:shadow-xl flex items-center gap-2">
+            <span>View Patient History</span>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 00-1-1H6zm0 2h8v1H6V4zm0 3h8v2H6V7zm0 3h5v2H6v-2z" />
+            </svg>
+          </button>
+        </Link>
+           <Link href="/videoUpload">
           <button className="px-8 py-4 bg-black hover:bg-gray-200 text-white rounded-lg 
             cursor-pointer text-lg font-semibold transition duration-300
             shadow-lg hover:shadow-xl flex items-center gap-2">
@@ -55,5 +65,6 @@ export default function Home() {
         </Link>
       </div>
     </main>
+
   );
 }
